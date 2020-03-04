6656e0d23b01744e68233bcf20952eb0d202fdb8
Tom Flídr <tomflidr@gmail.com>
2020-03-04 14:03:47 +0100

    json attribute parsing improvements

--- static/js/libs/Helpers.js
+++ static/js/libs/Helpers.js
@@ -1,40 +1,29 @@
 // get evaluated string helper
 var Helpers = {
-	getEvaluated: function(val){
-		var result;
-		var failResult = {
-			success: false,
-			data: val
+	getJsonFromString: function(val){
+		var result = {
+			success: true,
+			data: null,
+			message: null
 		};
 		if (String(val).length > 0) {
 			try{
-				result = {
-					success: true,
-					//data: eval('(function(){return ('+val+');})();')
-					data: JSON.parse(val)
-				}
+				result.data = JSON.parse(val);
 			}catch(e){
-				result = failResult;
+				result.success = false;
 				result.message = '[Evaluation error] ' + e.message;
 			}
 		} else {
-			result = failResult;
+			result = false;
 			result.message = '[Evaluation error] No data from: ' + arguments.callee.caller + '  from: ' + arguments.callee.caller.caller;
 		}
-		if (!result.success) {
-			log(result);
-		}
+		if (!result.success) 
+			console.log(result);
 		return result;
 	},
 	// get live javascript code from json_attr template helper
 	getJsonAttr: function (encodedValue) {
-		/*var decodeURIComponent(encodedValue)
-			.replace(/%(?=[\da-fA-F]{2})/gi, function () {
-				// PHP tolerates poorly formed escape sequences
-				return '%25'
-			})*/
-		
-		return Helpers.getEvaluated(
+		return Helpers.getJsonFromString(
 			decodeURIComponent(encodedValue)
 				.replace(/%(?=[\da-f]{2})/gi, function () {
 					// PHP tolerates poorly formed escape sequences
